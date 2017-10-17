0b64aeaf1d789fdbbcb358981f433094fec43df1
thecarterb <cbawsome77@gmail.com>
2017-10-16 21:24:41 -0400

    Use JSON format instead

--- site.rb
+++ site.rb
@@ -1,17 +1,18 @@
 require 'sinatra'
 require 'http'
+require 'json'
 
 METHOD = 'getQuote'.freeze
-FORMAT = 'text'.freeze
+FORMAT = 'json'.freeze
 KEY    = Random.new.rand(1..999999)
 LANG   = 'en'.freeze
 
 set :public_folder, File.join(File.dirname(__FILE__), '/static')
 
 get '/' do
-  quote = HTTP.get("http://api.forismatic.com/api/1.0/?method=#{METHOD}&format=#{FORMAT}&key=#{KEY}&lang=#{LANG}").to_s
+  quote = JSON.parse(HTTP.get("http://api.forismatic.com/api/1.0/?method=#{METHOD}&format=#{FORMAT}&key=#{KEY}&lang=#{LANG}").to_s)
   puts quote
-  AUTHOR = quote.split('  ')[1].gsub!('(', '').gsub!(')', '')
-  QUOTE  = quote.split('  ')[0]
+  AUTHOR = quote['quoteAuthor']
+  QUOTE  = quote['quoteText']
   erb :index
 end
